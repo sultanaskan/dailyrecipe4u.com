e915c469aad5dab221193faac97f8bb37bbc16c4
sultanaskan <sultanahmedaskan@gmail.com>
2026-02-20 14:25:02 +0600

    mongodb atlas database seted

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,8 +2,8 @@ import { MongoClient } from 'mongodb';
 
 const dbName = "dailyrecipe4u";
 // srv ছাড়া সরাসরি নোড ইউআরএল (পাসওয়ার্ডের জায়গায় আপনার পাসওয়ার্ড বসান)
-//const uri = "mongodb+srv://jakareatanmoy001_db_user:DLVjCkDQTKpCkQOp@cluster0.157bspu.mongodb.net/?appName=Cluster0";
-const uri = "mongodb://localhost:27017/dailyrecipe4u";
+const uri = "mongodb+srv://jakareatanmoy001_db_user:DLVjCkDQTKpCkQOp@cluster0.157bspu.mongodb.net/?appName=Cluster0";
+//const uri = "mongodb://localhost:27017/dailyrecipe4u";
 let db;
 
 async function connectDB() {
